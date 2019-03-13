e469213622649a31fc525a425b0fa25f28fa191c
amir shobak <36466070+amirshobak@users.noreply.github.com>
2019-03-13 22:00:07 +0200

    Add files via upload

---
+++ MCAL/PUR & PDR.c
@@ -0,0 +1,105 @@
+void Port_SetPinPullUp(uint8 port_index,uint8 pins_mask,uint8 enable){
+if(enable==1){
+	switch(port_index)
+	{
+		         case 0:
+			 GPIO_PORTA_PUR_R=GPIO_PORTA_PUR_R|pins_mask;
+				break;
+			case 1:
+			GPIO_PORTB_PUR_R=GPIO_PORTB_PUR_R|pins_mask;
+				break;
+			case 2:
+			GPIO_PORTC_PUR_R=GPIO_PORTC_PUR_R|pins_mask;
+				break;
+			case 3:
+			 GPIO_PORTD_PUR_R=GPIO_PORTD_PUR_R|pins_mask;
+				break;
+			case 4:
+			 GPIO_PORTE_PUR_R=GPIO_PORTE_PUR_R|pins_mask;
+				break;
+			case 5:
+			 GPIO_PORTF_PUR_R=GPIO_PORTF_PUR_R|pins_mask;
+				break;
+		}
+
+
+}
+if(enable==0){
+	switch(port_index)
+		{
+		         case 0:
+			 GPIO_PORTA_PUR_R=GPIO_PORTA_PUR_R&~pins_mask;
+				break;
+			case 1:
+			GPIO_PORTB_PUR_R=GPIO_PORTB_PUR_R&~pins_mask;
+				break;
+			case 2:
+			GPIO_PORTC_PUR_R=GPIO_PORTC_PUR_R&~pins_mask;
+				break;
+			case 3:
+			 GPIO_PORTD_PUR_R=GPIO_PORTD_PUR_R&~pins_mask;
+				break;
+			case 4:
+			 GPIO_PORTE_PUR_R=GPIO_PORTE_PUR_R&~pins_mask;
+				break;
+			case 5:
+			 GPIO_PORTF_PUR_R=GPIO_PORTF_PUR_R&~pins_mask;
+				break;
+		}
+	}
+
+}
+
+void Port_SetPinPulldown(uint8 port_index,uint8 pins_mask,uint8 enable){
+
+
+if(enable==1){
+	switch(port_index)
+	{
+	                 case 0:
+			 GPIO_PORTA_PDR_R=GPIO_PORTA_PDR_R|pins_mask;
+				break;
+			case 1:
+			GPIO_PORTB_PDR_R=GPIO_PORTB_PDR_R|pins_mask;
+				break;
+			case 2:
+			GPIO_PORTC_PDR_R=GPIO_PORTC_PDR_R|pins_mask;
+				break;
+			case 3:
+			 GPIO_PORTD_PDR_R=GPIO_PORTD_PDR_R|pins_mask;
+				break;
+			case 4:
+			 GPIO_PORTE_PDR_R=GPIO_PORTE_PDR_R|pins_mask;
+				break;
+			case 5:
+			 GPIO_PORTF_PDR_R=GPIO_PORTF_PDR_R|pins_mask;
+				break;
+	}
+	
+
+
+							}
+
+if(enable==0){
+	switch(port_index){
+			case 0:
+			 GPIO_PORTA_PDR_R=GPIO_PORTA_PDR_R&~pins_mask;
+				break;
+			case 1:
+			GPIO_PORTB_PDR_R=GPIO_PORTB_PDR_R&~pins_mask;
+				break;
+			case 2:
+			GPIO_PORTC_PDR_R=GPIO_PORTC_PDR_R&~pins_mask;
+				break;
+			case 3:
+			 GPIO_PORTD_PDR_R=GPIO_PORTD_PDR_R&~pins_mask;
+				break;
+			case 4:
+			 GPIO_PORTE_PDR_R=GPIO_PORTE_PDR_R&~pins_mask;
+				break;
+			case 5:
+			 GPIO_PORTF_PDR_R=GPIO_PORTF_PDR_R&~pins_mask;
+				break;
+			}
+		}
+									}
