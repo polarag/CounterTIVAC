c99bb1781c8651cdc56f9da2bd07eacc554c053b
Baher Abdelfatah <36481333+BaherAbdelfatah@users.noreply.github.com>
2019-03-15 18:44:58 +0200

    Add files via upload

---
+++ MCAL/types.h
@@ -0,0 +1,24 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef enum {STD_HIGH = 1, STD_LOW = 0} Dio_LevelType;
+typedef enum {PORT_PIN_IN, PORT_PIN_OUT} Port_PinDirectionType;
+typedef signed char 	int8_t;
+typedef unsigned char 	uint8_t;
+typedef signed short int 	int16_t;
+typedef unsigned short int 	uint16_t;
+typedef signed int 	int32_t;
+typedef unsigned int 	uint32_t;
+typedef unsigned long long 	uint64_t;
+typedef long long 	int64_t;
+typedef int8_t 	int8;
+typedef uint8_t 	uint8;
+typedef int16_t 	int16;
+typedef uint16_t 	uint16;
+typedef int32_t 	int32;
+typedef uint32_t 	uint32;
+typedef int64_t 	int64;
+typedef uint64_t 	uint64;
+
+
+#endif
