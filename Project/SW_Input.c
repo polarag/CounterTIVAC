603085c6ff86f02880da915589ccadbe28c87da1
polarag <polaragheb@gmail.com>
2019-03-15 18:47:07 +0200

    Update SW_Input.c

--- Project/SW_Input.c
+++ Project/SW_Input.c
@@ -1,1 +1,6 @@
-
+unsigned char SW_Input(uint8 port_index,uint8 pins_mask_I,uint8 pins_mask_D,uint8 pins_mask_R)
+{
+  if(DIO_ReadPort(port_index,pins_mask_I)>0) return 'I';
+  if(DIO_ReadPort(port_index,pins_mask_D)>0) return 'D';
+  if(DIO_ReadPort(port_index,pins_mask_R)>0) return 'R';
+}
