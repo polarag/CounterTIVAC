098b75826539b1944439a3be28b833638d68cac8
Hamzashobak <36466011+Hamzashobak@users.noreply.github.com>
2019-03-15 18:35:58 +0200

    Update DIO_FlipPort.c

--- MCAL/DIO_FlipPort.c
+++ MCAL/DIO_FlipPort.c
@@ -1,37 +1,36 @@
 
-void DIO_FlipPort (int uint8_Port_index,char uint8_Pins_Mask){
-	if (uint8_Port_index==0){
+void DIO_FlipPort ( uint8 Port_index, uint8 Pins_Mask){
+	if (Port_index==0){
 		
-		GPIO_PORTA_DATA_R =GPIO_PORTA_DATA_R^uint8_Pins_Mask;
+		GPIO_PORTA_DATA_R =GPIO_PORTA_DATA_R^Pins_Mask;
 	}
-	else if (uint8_Port_index==1){
+	else if (Port_index==1){
 		
-		GPIO_PORTB_DATA_R =GPIO_PORTB_DATA_R^uint8_Pins_Mask;
+		GPIO_PORTB_DATA_R =GPIO_PORTB_DATA_R^Pins_Mask;
 		
 	}
-	else if (uint8_Port_index==2)
+	else if (Port_index==2)
 	{
 		
-		GPIO_PORTC_DATA_R =GPIO_PORTC_DATA_R^uint8_Pins_Mask;
+		GPIO_PORTC_DATA_R =GPIO_PORTC_DATA_R^Pins_Mask;
 		
 	}
-	else if (uint8_Port_index==3){
+	else if (Port_index==3){
 		
-		GPIO_PORTD_DATA_R =GPIO_PORTD_DATA_R^uint8_Pins_Mask;
+		GPIO_PORTD_DATA_R =GPIO_PORTD_DATA_R^Pins_Mask;
 		
 	}
-	else if (uint8_Port_index==4)
+	else if (Port_index==4)
 	{
 		
-		GPIO_PORTE_DATA_R =GPIO_PORTE_DATA_R^uint8_Pins_Mask;
+		GPIO_PORTE_DATA_R =GPIO_PORTE_DATA_R^Pins_Mask;
 		
 	}
-	else if (uint8_Port_index==5)
+	else if (Port_index==5)
 	{
 		
-		GPIO_PORTF_DATA_R =GPIO_PORTF_DATA_R^uint8_Pins_Mask;
+		GPIO_PORTF_DATA_R =GPIO_PORTF_DATA_R^Pins_Mask;
 		
 	}
 }
-
 
