b26fa59434b8613965b527165156a073b769b6db
Baher Abdelfatah <36481333+BaherAbdelfatah@users.noreply.github.com>
2019-03-17 14:50:55 +0200

    Update SW_Init.c

--- Project/SW_Init.c
+++ Project/SW_Init.c
@@ -1,45 +1,9 @@
-void SW_Init(uint8 pins_mask,unit8 port_index)
+//Function to initialise switch As input and give it certain function for each switch
+void SW_Init(unsigned int pins_mask,unsigned int port_index)
 {
   uint8 enable=1;
-  switch (port_index)
-  {
-  		case 0://PortA
-      Port_Init(port_index);
-      Port_SetPinDirection(port_index,pins_mask,PORT_PIN_IN);
-      Port_SetPinPullUp(port_index,pins_mask,enable);
-  		break;
-
-  		case 1://PortB
-      Port_Init(port_index);
-      Port_SetPinDirection(port_index,pins_mask,PORT_PIN_IN);
-      Port_SetPinPullUp(port_index,pins_mask,enable);
-  		break;
-
-  		case 2://PortC
-      Port_Init(port_index);
-      Port_SetPinDirection(port_index,pins_mask,PORT_PIN_IN);
-      Port_SetPinPullUp(port_index,pins_mask,enable);
-  		break;
-
-  		case 3://PortD
-      Port_Init(port_index);
-      Port_SetPinDirection(port_index,pins_mask,PORT_PIN_IN);
-      Port_SetPinPullUp(port_index,pins_mask,enable);
-  		break;
-
-  		case 4://PortE
-      Port_Init(port_index);
-      Port_SetPinDirection(port_index,pins_mask,PORT_PIN_IN);
-      Port_SetPinPullUp(port_index,pins_mask,enable);
-  		break;
-
-  		case 5://PortF
-      Port_Init(port_index);
-      Port_SetPinDirection(port_index,pins_mask,PORT_PIN_IN);
-      Port_SetPinPullUp(port_index,pins_mask,enable);
-  		break;
-
-     // default:
-          // code to be executed if n doesn't match any constant
-  }
+  Port_Init(port_index);
+  Port_SetPinDirection(port_index,pins_mask,PORT_PIN_IN);
+  void Port_SetPinPullUp(port_index,pins_mask,enable);
+  void Port_SetPinPullDown(port_index,pins_mask,~enable);
 }
