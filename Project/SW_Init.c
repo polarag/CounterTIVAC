545ab30c0dfd220771ffd934df8a8e3d9da7cc4a
Baher Abdelfatah <36481333+BaherAbdelfatah@users.noreply.github.com>
2019-03-17 23:12:57 +0200

    Update SW_Init.c

--- Project/SW_Init.c
+++ Project/SW_Init.c
@@ -1,9 +1,10 @@
 //Function to initialise switch As input and give it certain function for each switch
-void SW_Init(unsigned int pins_mask,unsigned int port_index)
+void SW_Init(uint8 pins_mask,uint8 port_index)
 {
   uint8 enable=1;
-  Port_Init(port_index);
-  Port_SetPinDirection(port_index,pins_mask,PORT_PIN_IN);
-  void Port_SetPinPullUp(port_index,pins_mask,enable);
-  void Port_SetPinPullDown(port_index,pins_mask,~enable);
+      Port_Init_PIN(port_index,pins_mask);
+      Port_SetPinDirection(port_index,pins_mask,PORT_PIN_IN);
+      Port_SetPinPullUp(port_index,pins_mask,enable);
+	    Port_SetPinPullDown(port_index,pins_mask,~enable);
 }
+
